a3cc03da7e598ef21942d11f13ec521385ec38f6
HaoWang-TW <hawang@thoughtworks.com>
2018-05-19 21:34:05 +0800

    add tests for get B

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -6,4 +6,9 @@ describe('main()', () => {
         expect(main('2345', '2345')).toBe('4A0B');
     });
 
+    it('should get B if has correct number but wrong position', () => {
+        expect(main('1234', '1243').toBe('2A2B'));
+        expect(main('1234', '2134').toBe('2A2B'));
+        expect(main('2356', '3356').toBe('3A1B'));
+    })
 });
