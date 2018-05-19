0e46dbe0ea3b6101f14cc57725bc5eeb314eb7cb
HaoWang-TW <hawang@thoughtworks.com>
2018-05-19 23:36:21 +0800

    count A and B

--- main/main.js
+++ main/main.js
@@ -3,6 +3,22 @@ function main(expect, given) {
     if(expect === given) {
         return '4A0B';
     }
+
+    let abCount = {
+        A: 0,
+        B: 0
+    }
+
+    expect.split('')
+        .forEach((char, index) => {
+            if(char === given[index]) {
+                abCount.A++;
+            } else if(given.includes(char)) {
+                abCount.B++;
+            }
+        });
+
+    return `${abCount.A}A${abCount.B}B`;
 };
 
 module.exports = main
